ed3104106aa886c7c83a527748c5ac9c2da40bea
masingita makhubele <masingita@thedigitalacademy.co.za>
2020-07-20 12:09:29 +0200

    ..

---
+++ fe/src/app/mock-file.ts
@@ -0,0 +1,8 @@
+import {User} from './user'; 
+
+export const USERS: User[] = [
+    {name: 'sara' ,lastname:'hatlane', username:'sara@gmail.com', password: '12345' },
+    {name: 'victor' ,lastname:'vic', username:'victor@gmail.com', password: '1234' },
+    { name: 'masingita', lastname: 'makhubele', username: 'masingita@gmail.com', password: '123456' },
+    { name: 'eliot', lastname: 'manala', username: 'eliot@gmail.com', password: '1234567' }
+];
