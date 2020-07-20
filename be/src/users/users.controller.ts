584ceb11619cbc1223cef18cb4dc8a15aa5c3ca5
sarahatlane <sara@thedigitalacademy.co.za>
2020-07-20 10:46:48 +0200

    ../

--- be/src/users/users.controller.ts
+++ be/src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Get, Param, Post, Body, Delete, Put } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Users } from './users.model';
+​
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+ constructor(private readonly usersService: UsersService) {}
+​
+@Post()
+async register(
+    @Body('email') userEmail: string,
+    @Body('password') userPassword: string,
+) {
+    const generatedId = await this.usersService.registerUser(
+        userEmail,
+        userPassword
+    );
+ return { id: generatedId }
+ }
+}
