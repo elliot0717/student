8d4309bce67c4cbedc8cd5c0b165d975aa9c2f53
elliot0717 <fotune.manala@gmail.com>
2020-07-20 11:30:22 +0200

    Admin add component

--- fe/src/app/app-routing.module.ts
+++ fe/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { TestComponent } from './components/test/test.component';
+import { AdminAddComponent } from './components/admin-add/admin-add.component';
 
 // this constant default routes to Home, and also send all unknown path to Home
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'test', component: TestComponent },
+  { path: 'admin-add', component: AdminAddComponent },
  
   // default route
   { path: '', redirectTo: '/home', pathMatch: 'full' },
