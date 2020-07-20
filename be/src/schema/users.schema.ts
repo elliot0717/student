4ef762023627e79a8c3db2faa36562457f0449fd
sarahatlane <sara@thedigitalacademy.co.za>
2020-07-20 11:00:50 +0200

    fixing 2 errors

--- be/src/schema/users.schema.ts
+++ be/src/schema/users.schema.ts
@@ -1,13 +1,8 @@
 import * as mongoose from 'mongoose';
 ​
-export const UserSchema = new mongoose.Schema({
-    email: { type: String, require: true },
-    password: { type: String, require: true }
+export const UsersSchema = new mongoose.Schema({
+    fname: String,
+    lname: Number,
+    email: String,
+    password: String
 });
-​
-export interface Users {
-    firstname: String;
-    lastname: String
-    email: String;
-    password: String;
-}
