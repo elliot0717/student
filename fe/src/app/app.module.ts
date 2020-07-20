4c51431839d8fb5d2b51e351008cc22ef368845e
elliot0717 <fotune.manala@gmail.com>
2020-07-20 10:57:48 +0200

    Admin add component

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -11,13 +11,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TestComponent } from './components/test/test.component';
+import { AdminAddComponent } from './components/admin-add/admin-add.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    TestComponent
+    TestComponent,
+    AdminAddComponent
   ],
   imports: [
     BrowserModule,
