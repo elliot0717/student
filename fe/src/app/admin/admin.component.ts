01ac01ac53e2e27f5b6a8e457ca6b93a38de23b8
elliot0717 <fotune.manala@gmail.com>
2020-07-21 13:53:09 +0200

    ../

--- fe/src/app/admin/admin.component.ts
+++ fe/src/app/admin/admin.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import swal from 'sweetalert';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-admin',
@@ -8,25 +11,52 @@ import swal from 'sweetalert';
 })
 export class AdminComponent implements OnInit {
 
-  name: any;
-  faculties: string='';
+  university :any;
+  addForm : any;
+   // data type
+   id: any;
+   name: any;
+   faculty1: string='';
+   faculty2: string='';
+   faculty3: string='';
+   faculty4: string='';
+
+   //function for adding data on the table
+  addModalForm(AddForm, modalAdd){
+    console.log(this.name)
+    console.log( [this.faculty1, this.faculty2, this.faculty3, this.faculty4 ]),
+    this.university.push({ name: this.name, faculty1: this.faculty1 , faculty2: this.faculty2 , faculty3: this.faculty3, faculty4: this.faculty4});
+  
+  }
+
+
+
+
+  //function for edditing data on the table
+  EditModalForm(EditForm, modalEdit){
+    //console.log( this.id , this.name , this.email, this.IDNo, this.faculty1 ),
+   // this.university.fill({ name:this.name, email:this.email , IDNo:this.IDNo  , module:this.faculty1})
+    // swal(" Your imaginary Details has been edited!", {
+    //   icon: "success",
+    // });
+  }
+
+  Delete(){
+    console.log("Deleted")
+    // console.log( [this.id , this.name , this.email, this.IDNo, this.module ]),
+     //this.university.pop({id: this.id , name:this.name, email:this.email , IDNo:this.IDNo  , module:this.faculty1})
+    // this.students.pop=[{name: this.name, email: this.email , IDNo: this.IDNo  , module: this.module}]
+    // this.students = [{name: this.name, email: this.email , IDNo: this.IDNo  , module: this.module}]
+        // swal(" Your imaginary Details has been deleted!", {
+        //   icon: "success",
+        // });
+  }
+
+ 
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-   //function for adding data on the table
-   addModalForm(addForm, modalAdd){
-
-    console.log(this.name)
-    console.log( this.faculties)
-    //this.students.push({ name: this.name,  module: this.faculties })
-    // this.students=[{name: this.name, email: this.email , IDNo: this.IDNo  , module: this.module}],
-    // this.students = [{name: this.name, email: this.email , IDNo: this.IDNo  , module: this.module}]
-    swal(" Your imaginary Details has been Added!", {
-      icon: "success",
-    });
   }
 
 }
