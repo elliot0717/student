5dfff30ebffe1242c68db7b6cd45dc8f100f3ae8
sarahatlane <sara@thedigitalacademy.co.za>
2020-07-21 11:02:49 +0200

    ../

--- be/src/universities/universities.module.ts
+++ be/src/universities/universities.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UniversitiesService } from './universities.service';
+import { UniversitiesController } from './universities.controller';
 
-@Module({})
+@Module({
+  providers: [UniversitiesService],
+  controllers: [UniversitiesController]
+})
 export class UniversitiesModule {}
