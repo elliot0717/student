e7e916b888d2517998a6e52528c00555268de441
Emanuel Mahlangu <emanual@thedigitalacademy.co.za>
2020-07-22 14:22:13 +0200

    ../

--- fe/src/app/components/admin/admin.component.ts
+++ fe/src/app/components/admin/admin.component.ts
@@ -1,54 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
-import { ClassGetter } from '@angular/compiler/src/output/output_ast';
-
-
-
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { ClassGetter } from "@angular/compiler/src/output/output_ast";
+import { AdminService } from "src/app/admin/admin.service";
 
 @Component({
-  selector: 'app-admin',
-  templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  selector: "app-admin",
+  templateUrl: "./admin.component.html",
+  styleUrls: ["./admin.component.scss"],
 })
 export class AdminComponent implements OnInit {
+  university: any = [];
+  addForm: any;
+  modalAdd: any;
+  uniDetailsData = {};
+  // data type
+  id: any;
+  name: string;
+  faculty: string = "";
 
-  university :any = [];
-  addForm : any;
-  modalAdd : any;
-   // data type
-   id: any;
-   name: string;
-   faculty: string='';
-
-   //function for adding data on the table
-  addModalForm(addForm,modalAdd){
-    console.log(this.name)
-    console.log( this.faculty)
-    this.university.push({ name: this.name, faculty: this.faculty});
-  
+  //function for adding data on the table
+  addModalForm(addForm, modalAdd) {
+    console.log(this.name);
+    console.log(this.faculty);
+    this.university.push({ name: this.name, faculty: this.faculty });
   }
-
-
-
 
   //function for edditing data on the table
-  EditModalForm(EditForm, modalEdit){
-    console.log(this.name)
-    console.log( this.faculty)
-    this.university.fill({ name: this.name, faculty: this.faculty})
-
+  EditModalForm(EditForm, modalEdit) {
+    console.log(this.name);
+    console.log(this.faculty);
+    this.university.fill({ name: this.name, faculty: this.faculty });
   }
 
-  Delete(){
-    console.log("Deleted")
-    this.university.pop({ name: this.name, faculty: this.faculty});
+  Delete() {
+    console.log("Deleted");
+    this.university.pop({ name: this.name, faculty: this.faculty });
   }
 
- 
+  constructor(private adminService: AdminService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  addPost() {
+    this.uniDetailsData["universityName"] = this.name;
+    this.uniDetailsData["faculty"] = this.faculty;
 
-  ngOnInit(): void {
+    this.adminService
+      .addPosts(this.uniDetailsData)
+      .subscribe((res) => console.log(res), (err) => console.log(err));
   }
-
+  
 }
