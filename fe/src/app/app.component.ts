5519aa6ba4d9ff39ee962c712c41e83b7d38aef0
Emanuel Mahlangu <emanual@thedigitalacademy.co.za>
2020-07-23 08:14:48 +0200

    ../

--- fe/src/app/app.component.ts
+++ fe/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mdb-angular-free';
+export class AppComponent implements OnInit {
+
+  constructor (private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+   //this.http.get('http://localhost:3000/admin/All').subscribe(data => console.log(data));
+  }
+
+
 }
